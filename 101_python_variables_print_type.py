f30f66a7eb8fcc939e0ebe5a63f9d7942f1033cb
Elliot Harris <elliotharris@tcadmins-MacBook-Air.local>
2020-03-03 13:28:54 +0000

    Notes on variables, Print and Type

---
+++ 101_python_variables_print_type.py
@@ -0,0 +1,19 @@
+# What is a variable
+# it is like a box. You give it a name, put stuff inside
+#box = 'contents'
+book = 'Rich dad poor dad'
+
+
+# print function
+print(book)
+
+
+# type function
+#allows us to check data types
+data_of_book = type(book)
+print(data_of_book)
+
+
+
+# Conventions
+# lower case naming for variables with under scores for spacing
