16534ad46f8349a357196e18bb7d3e6402a75942
Elliot Harris <elliotharris@tcadmins-MacBook-Air.local>
2020-03-03 13:24:35 +0000

    Notes on Integers

---
+++ 103_integers.py
@@ -0,0 +1,24 @@
+# Numerical types
+# Intergers, Floats, complex numbers, big ints
+
+
+# integers
+# full numbers
+print(10)
+print(type(10))
+
+# floats
+# decimal numbers
+print(10.0)
+print(type(10.0))
+
+# they can be used together
+example = print(10/3)
+print(type(example))
+
+#they can be added, subratced, divided, multiplied
+
+# and modulus (%)
+# gives you remainder
+print(10%2) # no remainder
+print(10%3) # 3*3 is 9, one left over
