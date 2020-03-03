62be4e5d3d0bd7411af0d6a6bfc5f0e2d8ff7643
Elliot Harris <elliotharris@tcadmins-MacBook-Air.local>
2020-03-03 13:25:57 +0000

    Notes on booleans

---
+++ 104_booleans.py
@@ -0,0 +1,25 @@
+# boolean and comparison operators
+
+# boolean
+# it is a data type that is either True or False
+print(True)
+print(type(True))
+
+print(False)
+print(type(False))
+
+# comparison operators
+# they evaluate a code
+# they output ( ---> ) 'bool'
+num1=10
+num2=13
+
+# asserting if something is equal
+# use == to equate something
+print('==')
+print(10 == '10')
+
+# Greater than
+# Less than
+# greater than or equal to
+# less than or equal to
